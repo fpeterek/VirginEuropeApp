2767f23f3f54c1b4f7a4b3bd2a4afd7abe09c474
Filip Peterek <fpeterek@seznam.cz>
2020-05-11 14:43:55 +0200

    Styling

--- VirginEurope/forms.py
+++ VirginEurope/forms.py
@@ -8,25 +8,30 @@ TRAVEL_CLASS = [
 
 
 class OriginBox(forms.Form):
-    origin = forms.CharField(label='Origin', max_length=256, required=True,
-                             widget=forms.TextInput(attrs={'id': 'origin_box', 'class': 'ui-widget'}))
+    origin = forms.CharField(max_length=256, required=True, label='', widget=forms.TextInput(
+        attrs={'id': 'origin_box',
+               'class': 'airport-select ui-widget',
+               'placeholder': 'Origin'}))
 
 
 class DestinationBox(forms.Form):
-    destination = forms.CharField(label='Destination', max_length=256, required=True,
-                                  widget=forms.TextInput(attrs={'id': 'destination_box', 'class': 'ui-widget'}))
+    destination = forms.CharField(max_length=256, required=True, label='', widget=forms.TextInput(
+        attrs={'id': 'destination_box',
+               'class': 'airport-select ui-widget',
+               'placeholder': 'Destination'}))
 
 
 class BookFlightForm(forms.Form):
 
     date = forms.DateField(
-        label='Date',
-        widget=forms.DateInput(attrs={'id': 'flight_date', 'class': 'datepicker', 'data-toggle': 'datepicker'}),
+        label='',
+        widget=forms.DateInput(attrs={'id': 'flight_date', 'class': 'datepicker', 'data-toggle': 'datepicker',
+                                      'placeholder': 'Departure'}),
         required=True
     )
     cls = forms.ChoiceField(
-        label='Class',
+        label='',
         required=True,
-        widget=forms.Select,
+        widget=forms.Select(attrs={'class': 'class-select'}),
         choices=TRAVEL_CLASS,
     )
