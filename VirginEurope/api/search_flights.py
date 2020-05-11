5ddb7bec6553d6ebfa22b439e976e1c0bc5a111c
Filip Peterek <fpeterek@seznam.cz>
2020-05-11 16:22:47 +0200

    POST -> GET when searching for a flight

--- VirginEurope/api/search_flights.py
+++ VirginEurope/api/search_flights.py
@@ -120,7 +120,7 @@ def parse_flights(resp: str, cls: str) -> list:
 
 def search_flights(orig: str, dest: str, date: str, cls: str) -> list:
     data = {'orig': orig, 'dest': dest, 'date': date, 'cls': cls}
-    resp = requests.post(f'{Conf.url}/find-flights', json=data)
+    resp = requests.get(f'{Conf.url}/find-flights', params=data)
 
     if resp.status_code != 200:
         return []
