169f4a01a67e594874afe9313f93bbc48ea8737e
Filip Peterek <fpeterek@seznam.cz>
2020-05-11 16:06:00 +0200

    CSS

--- VirginEurope/api/search_flights.py
+++ VirginEurope/api/search_flights.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 import requests
@@ -21,13 +22,21 @@ class SingleSegment:
     def __init__(self, flight: str, flight_id: int, date: str, price: int, cls: str):
         self.flight = flight
         self.id = flight_id
-        self.date = date
+        self.date = datetime.datetime.strptime(date, '%Y-%m-%d')
         self.price = price
         self.cls = cls
 
     @staticmethod
     def parse_from(d: dict, cls: str):
         return SingleSegment(d['flight'], d['id'], d['date'], d['price'], cls)
+
+    @property
+    def pretty_departure(self) -> str:
+        return self.date.strftime('%d.%m.%Y')
+
+    @property
+    def pretty_flight(self) -> str:
+        return f'VU{int(self.flight[2:])}'
 
     def __str__(self):
         return f'Flight {self.flight} on {self.date}, travel class {self.cls}, price {self.price}'
@@ -36,15 +45,23 @@ class SingleSegment:
     def to_url_params(self) -> str:
         return _flight_to_url_params(first=self.id, cls=self.cls)
 
+    @property
+    def pretty_class(self) -> str:
+        return self.cls.capitalize()
+
+    @property
+    def is_single_segment(self) -> bool:
+        return True
+
 
 class TwoSegment:
     def __init__(self, flight1, id1, date1, flight2, id2, date2, price, cls):
         self.first_flight = flight1
         self.first_id = id1
-        self.first_date = date1
+        self.first_date = datetime.datetime.strptime(date1, '%Y-%m-%d')
         self.second_flight = flight2
         self.second_id = id2
-        self.second_date = date2
+        self.second_date = datetime.datetime.strptime(date2, '%Y-%m-%d')
         self.price = price
         self.cls = cls
 
@@ -61,6 +78,34 @@ class TwoSegment:
     @property
     def to_url_params(self) -> str:
         return _flight_to_url_params(first=self.first_id, second=self.second_id, cls=self.cls)
+
+    @property
+    def pretty_first_flight(self):
+        return f'VU{int(self.first_flight[2:])}'
+
+    @property
+    def pretty_second_flight(self):
+        return f'VU{int(self.second_flight[2:])}'
+
+    @property
+    def pretty_flights(self):
+        return f'{self.pretty_first_flight}, {self.pretty_second_flight}'
+
+    @property
+    def pretty_departure(self):
+        return self.first_date.strftime('%d.%m.%Y')
+
+    @property
+    def pretty_second_departure(self):
+        return self.second_date.strftime('%d.%m.%Y')
+
+    @property
+    def pretty_class(self) -> str:
+        return self.cls.capitalize()
+
+    @property
+    def is_single_segment(self) -> bool:
+        return False
 
 
 def parse_flights(resp: str, cls: str) -> list:
